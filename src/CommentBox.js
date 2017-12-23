ad3e3e777368eae59512a14e40b0038ee363efec
Joe Previte <jjprevite@gmail.com>
2017-12-23 09:04:58 -0700

    Add code to handle delete and submission

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,4 +1,3 @@
-//CommentBox.js
 import React, { Component } from 'react';
 import axios from 'axios';
 import CommentList from './CommentList';
@@ -11,6 +10,9 @@ class CommentBox extends Component {
         this.state = { data: [] };
         this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
         this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+        this.handleCommentDelete = this.handleCommentDelete.bind(this);
+        this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
+        this.pollInterval = null;
     }
     loadCommentsFromServer() {
         axios.get(this.props.url)
@@ -19,23 +21,54 @@ class CommentBox extends Component {
             })
     }
     handleCommentSubmit(comment) {
+        let comments = this.state.data;
+        comment.id = Date.now();
+        let newComments = comments.concat([comment]);
+        this.setState({ data: newComments });
         axios.post(this.props.url, comment)
+            .catch(err => {
+                console.error(err);
+                this.setState({ data: comments });
+            });
+    }
+    handleCommentDelete(id) {
+        axios.delete(`${this.props.url}/${id}`)
             .then(res => {
-                this.loadCommentsFromServer();
+                console.log('Comment deleted');
             })
             .catch(err => {
                 console.error(err);
             });
     }
+    handleCommentUpdate(id, comment) {
+        //sends the comment id and new author/text to our api
+        axios.put(`${this.props.url}/${id}`, comment)
+            .catch(err => {
+                console.log(err);
+            })
+    }
     componentDidMount() {
         this.loadCommentsFromServer();
-        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+        if (!this.pollInterval) {
+            this.pollInterval = setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+        }
+    }
+    //when incorporating into another project
+    //(with react-router for instance),
+    //this will prevent error messages every 2 seconds
+    //once the CommentBox is unmounted
+    componentWillUnmount() {
+        this.pollInterval && clearInterval(this.pollInterval);
+        this.pollInterval = null;
     }
     render() {
         return (
             <div style={style.commentBox}>
-                <h2>Comments:</h2>
-                <CommentList data={this.state.data} />
+                <h2 style={style.title}>Comments:</h2>
+                <CommentList
+                    onCommentDelete={this.handleCommentDelete}
+                    onCommentUpdate={this.handleCommentUpdate}
+                    data={this.state.data} />
                 <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
         )
