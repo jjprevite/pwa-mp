846546109097a3e4abf34f93289ec6a332d8dbe1
Joe Previte <jjprevite@gmail.com>
2017-12-22 17:18:51 -0700

    fix extra spaces

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //index.js
 import React from 'react';
-import ReactDOM from 'react - dom';
+import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 ReactDOM.render(
     <CommentBox />,
