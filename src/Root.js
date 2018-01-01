57b21bf5ebf4a4770fca9cf5981bcd93617fd7c2
Joe Previte <jjprevite@gmail.com>
2017-12-31 23:23:48 -0700

    Fix minor changes

--- src/Root.js
+++ src/Root.js
@@ -5,14 +5,14 @@ class Root extends Component {
     render() {
         return (
             <main>
-                <Route exact path="/" render={() => (
+                {/* <Route exact path="/" render={() => (
                     <AppBox
                         url='http://localhost:3001/api/apps'
                         pollInterval={2000} />
-                )}/>
+                )}/> */}
             </main>
         )
     }
 }
 
-export default AppList;
+export default Root;
