3ddddc25a845e33741d2b7caf154bccaedddeac4
Joe Previte <jjprevite@gmail.com>
2018-01-01 11:07:44 -0700

    Add Router element around root element

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 //index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppBox from './AppBox';
 
 ReactDOM.render(
+    <Router>
     <AppBox
         url='http://localhost:3001/api/apps'
-        pollInterval={2000} />,
+        pollInterval={2000} />
+    </Router>,
     document.getElementById('root')
 );
